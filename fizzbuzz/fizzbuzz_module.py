db2227b1c009e68e37289b4419062c9939ad0484
SZkristof <szabokristof0@gmail.com>
2017-03-27 13:46:59 +0200

    Working but 4 failures at test

--- fizzbuzz/fizzbuzz_module.py
+++ fizzbuzz/fizzbuzz_module.py
@@ -1,7 +1,14 @@
-def fizzbuzz(number):
-    return
-
-
+def fizzbuzz(number):     
+    numbers = list(range(1,int(number+1)))
+    for i in numbers:
+        if i % 3 ==0 and i % 5 ==0:
+            print ("'FizzBuzz'")
+        elif i % 3 ==0:
+            print ("'Fizz'")
+        elif i % 5 ==0:
+            print ("'Buzz'")
+        else:
+            print(str(i))
 def main():
     return
 
